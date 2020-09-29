a6bcbc571fd9c24a82047481aef3c10dd7a93b26
aha <ahxc1020@163.com>
2020-09-29 10:09:07 +0800

    增加指定路径保存

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 #### 使用方法
 
 ```python
-python main.py passwordName passwordNumber
+python main.py 密码名字 密码位数 [密码绝对路径不包括文件名]
 ```
 
 小记：每次改密码chrome提供的密码管理工具有时候有些网页调不出来，自己也懒得想就写了一个生成随机密码的小工具，运行后当前目录生成一个password.txt文件，可以追加编辑
--- main.py
+++ main.py
@@ -10,8 +10,12 @@ def joinS(a, b):
 
 passwordName = sys.argv[1]
 passwordNumber = sys.argv[2]
+if len(sys.argv) >= 4:
+	passwordPath = os.path.join(sys.argv[3], "password.txt")
+else:
+	passwordPath = "password.txt"
 password = reduce(joinS, random.sample(choice, int(passwordNumber)))
 password = passwordName + "：" + password
 
-with open("password.txt", 'a', encoding="utf-8") as f:
+with open(passwordPath, 'a', encoding="utf-8") as f:
 	f.write(password + '\n')
